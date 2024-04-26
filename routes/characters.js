55379e8be16b9a51e4f2716184f8001ef9808d6a
ivanueLDGR <ivanue2810@gmail.com>
2024-04-26 15:32:02 -0300

    create field validation logic, apply validation logic to POST, update some character fields to english

--- routes/characters.js
+++ routes/characters.js
@@ -23,6 +23,75 @@ async function writeCharactersList(data){
     }
 }
 
+function validatePost(newCharacter){
+    let statusArray = []
+    const newCharacterDesconstructed = {...newCharacter}
+    statusArray.push(validateName(newCharacterDesconstructed.name))
+    statusArray.push(validateClasse(newCharacterDesconstructed.classe))
+    statusArray.push(validateHabilidades(newCharacterDesconstructed.habilidades))
+    statusArray.push(validateAge(newCharacterDesconstructed.age))
+    statusArray.push(validateItens(newCharacterDesconstructed.itens))
+    return statusArray
+}
+
+function validateName(name){
+    if (!name){
+        return {status: "missing", field: "name"};
+    }else if (typeof name != "string"){
+        return {status: "incorrect", field: "name"}
+    }
+    return {status: "ok", field: "name"}
+}
+
+function validateClasse(classe){
+    if (!classe){
+        return {status: "missing", field: "classe"};
+    }else if (typeof classe != "string"){
+        return {status: "incorrect", field: "classe"}
+    }
+    return {status: "ok", field: "classe"}
+}
+
+function validateHabilidades(habilidades){
+    if (!classe){
+        return {status: "missing", field: "habilidades"};
+    }else if (typeof habilidades != "array"){
+        return {status: "incorrect", field: "habilidades"}
+    }
+    return {status: "ok", field: "habilidades"}
+}
+
+function validateAge(age){
+    if (!age){
+        return {status: "missing", field: "age"};
+    }else if (typeof age != "number" || age < 1){
+        return {status: "incorrect", field: "age"}
+    }
+    return {status: "ok", field: "age"}
+}
+
+function validateItens(itens){
+    if (!itens){
+        return {status: "missing", field: "itens"};
+    }else if (typeof itens != "array"){
+        return {status: "incorrect", field: "itens"}
+    }
+    return {status: "ok", field: "itens"}
+}
+
+function checkStatusArray(statusArray){
+    let statusMessage = ""
+    for (var i in statusArray){
+        if(statusArray[i].status != "ok"){
+            statusMessage = statusMessage + `your ${Object.values(statusArray[i])[1]} is ${Object.keys(statusArray[i])[0]}, `
+        }
+    }
+    if (statusMessage == ""){
+        statusMessage = "ok"
+    }
+    return statusMessage
+}
+
 router.get("/", async (req, res) => {
     try {
         const data = await readCharactersList();
@@ -36,9 +105,14 @@ router.post("/", async (req, res) => {
     try {
         const data = await readCharactersList();
         const newCharacter = req.body
+        const statusArray = validatePost(newCharacter)
+        const statusMessage = checkStatusArray(statusArray)
+        if (statusMessage != "ok"){
+            return res.send(statusMessage)
+        }
         data.push(newCharacter)
         await writeCharactersList(data)
-        res.send(data)
+        return res.send("Character was successfully created")
     } catch (error) {
         console.log(error);
     }
